ee8181b47d0c39ff543e8a0859d50103e9a70d07
user <user@DESKTOP-UF4D6BV>
2020-04-02 22:42:34 +0900

    수정

---
+++ uhd/src/main/java/com/example/uhd/TestController.java
@@ -0,0 +1,12 @@
+package com.example.uhd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping("/")
+    public String hello() {
+        return "test1";
+    }
+}
